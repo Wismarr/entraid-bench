73a8cb99e728771f25fee7bbe6d35fc623e05827
Wismarr <wrivas@consein.com>
2024-11-13 12:14:31 -0400

    ACTUALIZACIONES al 13 11

---
+++ controls/Test-OnPremisesSync.ps1
@@ -0,0 +1,38 @@
+function Test-OnPremisesSync {
+    [CmdletBinding()]
+    param()
+
+    try {
+
+        $controlTitle = "Ensure that password hash sync is enabled for hybrid deployments"
+        $controlDescription = "Password hash synchronization helps by reducing the number of passwords your users need to maintain to just one and enables leaked credential detection for your hybrid accounts. Leaked credential protection is leveraged through Azure AD Identity Protection and is a subset of that feature which can help identify if an organization's user account passwords have appeared on the dark web or public spaces. Using other options for your directory synchronization may be less resilient as Microsoft can still process sign-ins to 365 with Hash Sync even if a network connection to your on-premises environment is not available."
+    
+        $organization = Get-MgOrganization
+        $OnPremisesSyncEnabled = $organization.OnPremisesSyncEnabled
+        $OnPremisesLastSyncDateTime = $organization.OnPremisesLastSyncDateTime
+
+        if ($OnPremisesSyncEnabled) {
+            $controlFinding = "On-Premises Sync is enabled."
+            $complianceResult = "COMPLIANT"
+        } else {
+            $controlFinding = "On-Premises Sync is not enabled."
+            $complianceResult = "NOT COMPLIANT"
+        }
+
+        return [PSCustomObject]@{
+            Control               = $controlTitle
+            ControlDescription    = $controlDescription
+            Finding               = $controlFinding
+            OnPremisesSyncEnabled = $OnPremisesSyncEnabled
+            LastSyncDateTime      = $OnPremisesLastSyncDateTime
+            Result                = $complianceResult
+        }
+    
+    } catch {
+        Write-Error "An error occurred: $_"
+    }
+}
+
+
+# Usage in your scripts:
+Test-OnPremisesSync
